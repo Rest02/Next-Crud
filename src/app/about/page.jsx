e502ed535c2d0194a4a0a4d74a4c29db4ea147e6
Rest <code.rest.dev@gmail.com>
2025-03-11 16:25:27 -0300

    Navegación

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <section className='container mx-auto'>
+      <h3 className='text-3xl font-bold my-5'>About</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit obcaecati placeat eveniet nisi autem. Unde consequuntur iste nam voluptates adipisci maiores laudantium optio amet sed voluptas, neque culpa odio molestiae, id, debitis reprehenderit. Debitis cumque tenetur impedit! Aspernatur odio quam dolore, vitae possimus iusto libero facilis dignissimos consectetur! Amet aspernatur vitae quod tenetur assumenda maxime suscipit soluta quos tempora sequi, facilis quasi blanditiis temporibus, atque, ducimus alias! Amet nostrum sunt reprehenderit nesciunt placeat blanditiis quisquam quibusdam aut harum id possimus cum, eaque corrupti dolore expedita, alias perspiciatis deserunt necessitatibus architecto officia. Possimus nihil temporibus vero odio magni iure cumque debitis.</p>
+    </section>
   )
 }
 
