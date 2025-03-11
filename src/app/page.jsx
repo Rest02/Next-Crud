00ff68bf01b5418c296039abab617834427e6ed6
Rest <code.rest.dev@gmail.com>
2025-03-11 00:41:28 -0300

    Renderizar tareas

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,24 @@
+import { prisma } from "@/libs/prisma";
 
-function HomePage() {
-  return (
-    <div>Tareas</div>
-  )
+async function loadTask() {
+  return await prisma.task.findMany();
+  
 }
 
-export default HomePage
+async function HomePage() {
+  const tasks = await loadTask();
+  return <section className="container mx-auto">
+    <div className="grid grid-cols-3 gap-3 mt-10">
+    {tasks.map((task) =>(
+      <div key = {task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
+        <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+        <p>{task.description}</p>
+        <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+
+      </div>
+    ))}
+  </div>
+  </section>
+}
+
+export default HomePage;
