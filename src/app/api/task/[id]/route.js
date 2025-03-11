5dacdd90e6060624d2c360e6f71c51648cb7d0df
Rest <code.rest.dev@gmail.com>
2025-03-10 22:40:54 -0300

    NextJs REST API

---
+++ src/app/api/task/[id]/route.js
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
+
+export async function GET(request, { params }) {
+  const { id } = await params;
+
+  const task = await prisma.task.findUnique({
+    where: {
+      id: Number(id),
+    },
+  });
+
+  return NextResponse.json(task);
+}
+
+export async function PUT(request, { params }) {
+  const { id } = await params;
+
+  const { title, description } = await request.json();
+  const taskUpdated = await prisma.task.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      title,
+      description,
+    },
+  });
+
+  return NextResponse.json(taskUpdated);
+}
+
+export async function DELETE(request, { params }) {
+  const { id } = await params;
+
+  try {
+    const taskRemoved = await prisma.task.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    console.log(taskRemoved);
+  } catch (error) {
+    return NextResponse.json(error.message);
+  }
+
+  return NextResponse.json(taskRemoved);
+}
